554f799641761d18f29df8e0d268eea0633b0a65
shamar12334 <weekesshamar38@gmail.com>
2025-09-20 14:43:06 -0400

    Set the const to handle name change,password change and delete account.

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React,{useState} from 'react'
 
-function Settings() {
-  return (
-    <div>Settings</div>
-  )
-}
+const Settings = () =>
+  {
+    const [name,setName] = useState("John","Doe");
+    const [newName, setNewName] =useState(name);
+    const [password,setPassword] = useState("");
+    const[newPassword,SetNewPassword] = useState("");
+
+    const handleNameChange = (e) =>
+      {
+        e.preventDefault();
+      };
+    const handlePasswordCHange = (e) =>
+    {
+      e.preventDefault();
+      if(password !== newPassword)
+      {
+        alert("password does not match");
+      }
+      setPassword("");
+      SetNewPassword("");
+
+    };
+
+    const handleDeleteAccount =(e) =>
+    {
+      const check = window.confirm("Do you want to deactivate your account");
+      if(check)
+      {
+        alert("Account deleted");
+      }
+    };
+    return(
+      <div className='min-h-screen bg-[#0F1924]'>
+
+      </div>
+    );
+  }
 
 export default Settings
